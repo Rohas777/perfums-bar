23b1c6335c4c5d82fbe6aba570dc170f523f364d
Pislyakov Oleg <olegpislyakov@mail.ru>
2024-06-16 01:23:41 +0300

    contacts page

---
+++ js/contacts.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    //NOTE - Сброс состояния ошибки у инпутов при вводе
+
+    $("input").on("input", removeInputErrorState);
+
+    //NOTE - Форма подписки в UTP2
+
+    $("#follow").submit(function (e) {
+        e.preventDefault();
+
+        const inputs = [
+            {
+                name: "email",
+                elem: $("#follow-email"),
+                value: $("#follow-email").val().trim(),
+                errorText: "Пожалуйста, укажите ваш Email",
+            },
+        ];
+        console.log(checkForm(inputs));
+    });
+
+    //NOTE - Форма заявки вопроса в UTP2
+
+    $("#feedback").submit(function (e) {
+        e.preventDefault();
+
+        const inputs = [
+            {
+                name: "name",
+                elem: $("#feedback-name"),
+                value: $("#feedback-name").val().trim(),
+                errorText: "Пожалуйста, укажите ваше имя",
+            },
+            {
+                name: "tel",
+                elem: $("#feedback-tel"),
+                value: $("#feedback-tel").val().trim(),
+            },
+        ];
+
+        console.log(checkForm(inputs));
+    });
+});
