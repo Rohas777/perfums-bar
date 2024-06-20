189a50c94dc8056d8fd9e5d87a4b6f242d1c200a
Pislyakov Oleg <olegpislyakov@mail.ru>
2024-06-21 02:11:46 +0300

    contacts page responsive

--- js/about.js
+++ js/about.js
@@ -1,28 +1,63 @@
 $(document).ready(function () {
-    //NOTE - Слайдер отзывов
-    
-    const reviewsSwiper = new Swiper(".reviews__slider", {
-        loop: true,
-        navigation: {
-            nextEl: ".reviews__nav .swiper-button-next",
-            prevEl: ".reviews__nav .swiper-button-prev",
-        },
-        pagination: {
-            el: ".reviews .swiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            0: {
-                spaceBetween: 30,
-                slidesPerView: 1,
-            },
-            601: {
-                slidesPerView: 3,
-            },
-            993: {
-                spaceBetween: 40,
-            },
-        },
+    //NOTE - Подгонка оригинальных размеров изображений под vw значения
+
+    $(".content img").each(function () {
+        let initWidth = $(this).width(),
+            viewportWidth = $("body").width(),
+            calcWidth = (initWidth / viewportWidth) * 100;
+
+        $(this).css("width", calcWidth + "vw");
     });
+
+    //NOTE - Слайдер отзывов
+
+    
+
+    const reviewsSwiper = new Swiper(".reviews__slider", {
+
+        loop: true,
+
+        navigation: {
+
+            nextEl: ".reviews__nav .swiper-button-next",
+
+            prevEl: ".reviews__nav .swiper-button-prev",
+
+        },
+
+        pagination: {
+
+            el: ".reviews .swiper-pagination",
+
+            clickable: true,
+
+        },
+
+        breakpoints: {
+
+            0: {
+
+                spaceBetween: 30,
+
+                slidesPerView: 1,
+
+            },
+
+            601: {
+
+                slidesPerView: 3,
+
+            },
+
+            993: {
+
+                spaceBetween: 40,
+
+            },
+
+        },
+
+    });
+
     
 });
